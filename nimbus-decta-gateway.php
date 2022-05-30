afc26640154ac321b30c8de8dcca76968f0169f9
Jack <jack@nimbusvapour.com.au>
2022-05-30 22:28:37 +1000

    testing live form

--- nimbus-decta-gateway.php
+++ nimbus-decta-gateway.php
@@ -2,7 +2,7 @@
 /*
 PLugin Name: NimbusDectaGateway
 Description: NimbusDecta WooCommerce Payment Gateway
-Version: 1.0
+Version: 0.1
 Author: Jack Tubbenhauer
 AUthor URI:
 Copyright: 2022 Jack Tubbenhauer
@@ -35,6 +35,7 @@ function init_nimbus_gateway_class() {
         $this->id = 'nimbusgatway';
         $this->method_title = 'Nimbus Gateway';
         $this->icon = '';
+        $this->has_fields = true;
         $this->init_form_fields();
         $this->init_settings();
         $this->title = 'Card Payment';
@@ -42,6 +43,29 @@ function init_nimbus_gateway_class() {
 
         add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
     }
+
+    public function payment_fields() {
+        if ( $this->description ) {
+          echo wpautop(wp_kses_post($this->description));
+        }
+
+        echo '<fieldset id="wc-' . esc_attr($this->id) . 'cc-form" class="wc-credit-card-form wc-payment-form" style="background:transparent;">';
+        echo '<div class="form-row form-row-wide"><label>Card Number <span class="required">*</span></label>
+        <input id="nimbus_ccNum" type="text" autocomplete="off">
+        </div>
+        <div class="form-row form-row-first">
+          <label>Expiry Date <span class="required">*</span></label>
+          <input id="nimbus_expdate" type="text" autocomplete="off" placeholder="MM/YY">
+        </div>
+        <div class="form-row form-row-last">
+          <label>Card Code (CVV) <span class="required">*</span></label>
+          <input id="nimbus_cvv" type="password" autocomplete="off" placeholder="CVC">
+        </div>
+        <div class="clear"></div>';
+
+        echo '<div class="clear"></div></fieldset>';
+    }
+
 
     public function init_form_fields() {
         $this->form_fields = array(
