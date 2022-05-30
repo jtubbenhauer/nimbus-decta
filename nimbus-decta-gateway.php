a37c0b87b7b50c92e269780ecf788630e616779c
jtubbenhauer <jack@nimbusvapour.com.au>
2022-05-30 17:12:01 +1000

    starting wc docs

---
+++ nimbus-decta-gateway.php
@@ -0,0 +1,26 @@
+<?php 
+/*
+PLugin Name: NimbusDectaGateway
+Description: NimbusDecta WooCommerce Payment Gateway
+Version: 1.0
+Author: Jack Tubbenhauer
+AUthor URI:
+Copyright: 2022 Jack Tubbenhauer
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+//https://woocommerce.com/document/payment-gateway-api/
+
+require_once 'novatiipayments-api.php';
+require_once 'logger.php';
+
+add_action('plugins_loaded', 'init_nimbus_gateway');
+
+function init_nimbus_gateway() {
+    // https://woocommerce.github.io/code-reference/classes/WC-Payment-Gateway.html
+    class WC_Nimbus_Gateway extends WC_Payment_Gateway {
+
+    }
+}
+
